b31c5dcc88e127687699ea3824083fbcd3109e75
peter-sigma <petergichehacrypto@gmail.com>
2024-07-02 10:14:10 +0000

    revise task 0

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,19 @@
-export default function getListStudents() {
-  const studentOne = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
-  const studentTwo = { id: 2, firstName: 'James', location: 'Columbia' };
-  const studentThree = { id: 5, firstName: 'Serena', location: 'San Francisco' };
+const getListStudents = () => [
+  {
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisco',
+  },
+  {
+    id: 2,
+    firstName: 'James',
+    location: 'Columbia',
+  },
+  {
+    id: 5,
+    firstName: 'Serena',
+    location: 'San Francisco',
+  },
+];
 
-  return new Array(studentOne, studentTwo, studentThree);
-}
+export default getListStudents;
