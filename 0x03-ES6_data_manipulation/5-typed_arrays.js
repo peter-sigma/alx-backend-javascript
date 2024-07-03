8c8e85665865ce0ca66291b593164e142fd84230
peter-sigma <petergichehacrypto@gmail.com>
2024-07-03 01:55:37 +0000

    update 5-typed_arrays.js

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,13 +1,14 @@
-export default function createInt8TypedArray(length, position, value) {
-  if (typeof length !== 'number' || typeof position !== 'number' || typeof value !== 'number') {
+const createInt8TypedArray = (length, position, value) => {
+  const buffer = new ArrayBuffer(length);
+  const data = new DataView(buffer);
+
+  if (position > length - 1) {
     throw new Error('Position outside range');
   }
-  if (position >= length) {
-    throw new Error('Position outside range');
-  }
-  const buff = new ArrayBuffer(length);
-  const nView = new DataView(buff, 0, length);
-  const arr = new Int8Array(buff);
-  arr[position] = value;
-  return nView;
-}
+
+  data.setInt8(position, value);
+
+  return data;
+};
+
+export default createInt8TypedArray;
